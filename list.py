ed45003604e9c8fcaacb510287713f0f86d33aaa
your-name <your-email-address>
2022-02-08 10:21:40 +0300

    lists are Mutable/changable

--- list.py
+++ list.py
@@ -6,5 +6,19 @@ for friend in friends :
 print('Done')
 '''
 #looking inside lists
+'''
 friends = ['roy', 'keith', 'william']
 print(friends[1])
+'''
+#lists are Mutable/changable
+fruit = 'Banana'
+fruit[0] = 'b'
+x = fruit.lower()
+print(x)
+
+lotto = [2, 14, 25, 41, 65]
+print(lotto)
+lotto[2] = 28
+
+print(lotto)
+
